c3ec6280d11e3d4657fa966c01884924395673e4
Zhicong <zhicong37@gmail.com>
2019-10-30 18:03:00 -0500

    bisect search on function instead of array

--- R/bisect.R
+++ R/bisect.R
@@ -1,22 +1,38 @@
-bisect <- function(arr,target,right = TRUE,index = TRUE){
-  n <- length(arr)
-  if(target > arr[n] || target < arr[1])
-    stop("`target` out of range of `arr`", call. = F)
-
-  lower <- 1
-  upper <- n
-
-  if(right){
-    while(lower<upper){
-      temp <- floor((lower+upper)/2)
-      if(arr[temp] < target) lower <- temp+1 else upper <- temp
+bisect <- function(fn, lower, upper, discrete = TRUE,
+                   target, right = TRUE,index = TRUE,...){
+  if(target > fn(upper) || target < fn(lower))
+    stop("`target` out of range of `fn`", call. = F)
+  if(discrete){
+    if(!exists("unit",inherits = F)) unit <- 1
+    gn <- function(x) fn(x*unit+lower)
+    lb <- 0
+    ub <- floor((upper-lower)/unit)
+    if(right){
+      while(lb<ub){
+        temp <- floor((lb+ub)/2)
+        if(gn(temp) < target) lb <- temp+1 else ub <- temp
+      }
+    }else{
+      while(lb<ub){
+        temp <- ceiling((lb+ub)/2)
+        if(gn(temp) <= target) lb <- temp else ub <- temp-1
+      }
     }
+    if(index) return(lb*unit+lower) else return(gn(lb))
   }else{
-    while(lower<upper){
-      temp <- ceiling((lower+upper)/2)
-      if(arr[temp] <= target) lower <- temp else upper <- temp-1
+    if(!exists("tol",inherits = F)) tol <- 10^(-7)
+    if(right){
+      while(lower < upper-tol){
+        temp <- (lower+upper)/2
+        if(fn(temp) < target) lower <- temp else upper <- temp
+      }
+      if(index) return(upper) else return(fn(upper))
+    }else{
+      while(lower < upper-tol){
+        temp <- (lower+upper)/2
+        if(fn(temp) <= target) lower <- temp else upper <- temp
+      }
+      if(index) return(lower) else return(fn(lower))
     }
   }
-
-  if(index) return(lower) else return(arr[lower])
 }

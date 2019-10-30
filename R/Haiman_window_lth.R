6711dfee11e0668dcefe7920aa9391d43f5f1a4a
Zhicong <zhicong37@gmail.com>
2019-10-30 18:04:52 -0500

    apply bisect here. number of 1-dependent sequence should be N/window_l becasue when apply `Haiman_window_length`, the input set as N-1.

--- R/Haiman_window_lth.R
+++ R/Haiman_window_lth.R
@@ -2,7 +2,7 @@ Haiman_window_lth <- function(N, p, alpha, lower_wl=3, upper_wl=100){
   check_fun <- function(N, critical_cnt, window_l, p, alpha){
     p1 <- 1 - AnomDetct::q1_function(critical_cnt, window_l, p)
 
-    n <- floor(N/window_l-1)
+    n <- floor(N/window_l)
     return(p1 <= 0.025 &
              n>3 &
              3.3*n*p1^2 < 1 &
@@ -13,28 +13,8 @@ Haiman_window_lth <- function(N, p, alpha, lower_wl=3, upper_wl=100){
   upper_wl <- min(ceiling(N/4),upper_wl)
 
   if(lower_wl>upper_wl) stop("Inappropriate `lower_wl`/`upper_wl`", call. = F)
-  if(lower_wl==upper_wl){
-    if(check_fun(N = N, critical_cnt = lower_wl-1,
-                 window_l = lower_wl, p = p, alpha = alpha)){
-      return(lower_wl)
-    }else{
-      stop("No result in given range", call. = F)
-    }
-  }
-  while(lower_wl<upper_wl){
-    window_l_Haiman <- floor(mean(c(lower_wl,upper_wl)))
-    satis <- check_fun(N = N,
-                       critical_cnt = window_l_Haiman-1,
-                       window_l = window_l_Haiman,
-                       p = p,
-                       alpha = alpha)
-    if(satis){
-      upper_wl <- window_l_Haiman
-    }else{
-      window_l_Haiman <- window_l_Haiman + 1
-      lower_wl <- window_l_Haiman
-    }
-  }
-
-  return(window_l_Haiman)
+  fn <- function(x) check_fun(N = N, critical_cnt = x-1, window_l = x,
+                              p = p, alpha = alpha)
+  return(AnomDetct::bisect(fn = fn, lower = lower_wl, upper = upper_wl,
+                           target = TRUE))
 }

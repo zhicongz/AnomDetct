c5c695899624167d820b6ccc3d00076092a5111d
Zhicong <zhicong37@gmail.com>
2019-11-14 19:08:37 -0600

    q(k) is defined as probability cnt <= k, so the complement should be cnt>k. From the simulation study, cnt>=k comes with larger false positive than expected

--- R/hypo_test.R
+++ R/hypo_test.R
@@ -5,7 +5,7 @@ hypo_test <- function(cdf, HRR = rep(1,length(cdf)-1),
   indicator <- cdf[2:N] <= expd_cdf
 
   cnt <- AnomDetct::rolling_sum(temp = indicator, window_l = window_l)
-  clst_loc <- which(cnt >= critical_cnt)
+  clst_loc <- which(cnt > critical_cnt)
 
   start_index <- c(clst_loc[1],
                    ifelse(diff(clst_loc) < window_l, NA, clst_loc[-1])) + 1
